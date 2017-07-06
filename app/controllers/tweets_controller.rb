9249b2d8244a05b693addd04391f3969b484f750
Samson Yuwono <samson.yuwono@gmail.com>
2017-07-06 18:28:00 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,13 @@
 class TweetsController < ApplicationController
+
+  get 'tweets/new' do
+    erb :'/tweets/new'
+  end
+
+  post 'tweets' do
+      @tweet= Tweet.create(content: params["content"])
+      @tweet.save
+      redirect("/tweets/#{@tweet.id}")
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+enable :sessions
+
+  get 'signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if user.username.empty? || user.password.nil?
+      redirect "/failure"
+    else
+      user.save
+      redirect "/login"
+    end
+  end
+end
+
+
+
+helpers do
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    User.find(session[:user_id])
+  end
 end
