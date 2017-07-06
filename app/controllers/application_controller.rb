e97b8112ef54fc7507136b3e730e37c059c0392c
Samson Yuwono <samson.yuwono@gmail.com>
2017-07-06 22:07:15 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,52 @@ enable :sessions
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
+    set :session_secret, "password_security"
   end
 
   get '/' do
     erb :index
   end
 
+  get '/tweets' do
+    @tweets= Tweet.all
+    @user = current_user
+    erb :'/tweets/tweets'
+  end
+
+  #show
+  get 'tweets/:id' do
+
+  end
+
+  #edit
+  get 'tweets/:id/edit' do
+
+  end
+
+  post 'tweets/:id' do
+
+  end
+
+  post '/tweets' do
+    @tweet= Tweet.create(content: params["content"])
+    @tweet.save
+    redirect("/tweets/#{@tweet.id}")
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if user.username.empty? || user.password.nil?
+      redirect "/failure"
+    else
+      user.save
+      redirect "/login"
+    end
+  end
 
   helpers do
     def logged_in?
